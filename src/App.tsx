7ab7de4d3ce001add4a5de5e6d22b7a0d096bad1
Dêvid Teófilo <nnick4673@gmail.com>
2025-03-27 19:32:27 -0300

    hello world in react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
+        repudiandae cumque illo ea deleniti ex velit placeat, nihil hic, tenetur
+        magnam deserunt fugit labore. Nisi voluptatem sequi quas odio quam.
+      </p>
+    </>
+  );
+}
