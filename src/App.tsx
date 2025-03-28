f7a9787015136c4b54f219a027fac8bb0b91a2d8
Dêvid Teófilo <nnick4673@gmail.com>
2025-03-27 21:00:41 -0300

    adding module css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { Heading } from "./components/Heading";
+
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  return (
-    <>
-      <h1>Olá mundo!</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-        repudiandae cumque illo ea deleniti ex velit placeat, nihil hic, tenetur
-        magnam deserunt fugit labore. Nisi voluptatem sequi quas odio quam.
-      </p>
-    </>
-  );
+    return (
+        <>
+            <Heading />
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Excepturi repudiandae cumque illo ea deleniti ex velit placeat,
+                nihil hic, tenetur magnam deserunt fugit labore. Nisi voluptatem
+                sequi quas odio quam.
+            </p>
+        </>
+    );
 }
