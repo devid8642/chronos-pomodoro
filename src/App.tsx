2992a5132ee97c6318c4885f928d67f6fb9ab1a9
Dêvid Teófilo <nnick4673@gmail.com>
2025-04-01 21:11:57 -0300

    estrutura básica das páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Timer } from "lucide-react";
 
 export function App() {
     return (
         <>
-            <Heading>
-                Olá Mundo!
-                <button>
-                    <Timer />
-                </button>
-            </Heading>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Excepturi repudiandae cumque illo ea deleniti ex velit placeat,
-                nihil hic, tenetur magnam deserunt fugit labore. Nisi voluptatem
-                sequi quas odio quam.
-            </p>
+            <div className="container">
+                <div className="content">
+                    <section>LOGO</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>MENU</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>FORM</section>
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="content">
+                    <section>FOOTER</section>
+                </div>
+            </div>
         </>
     );
 }
